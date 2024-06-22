89fcfd1406c5053df507ba827b83690545029f36
VictoriaP <sv184985@gmail.com>
2024-06-22 10:42:42 -1200

    'agregado database'

--- entornoTPbackend/app/database.py
+++ entornoTPbackend/app/database.py
@@ -25,8 +25,11 @@ def close_db(e=None):
 # Extraer la conexión a la base de datos de 'g' y eliminarla
     db = g.pop('db', None)
 # Si la conexión existe, cerrarla
-if db is not None:
-    db.close()
+
+#ACA APARECIA QUE DB NO EXISTIA, POR LO QUE AGREGUE g.db, NO SE SI CORRESPONDE
+
+if g.db is not None:
+    g.db.close()
 # Función para inicializar la aplicación con el manejo de la base de datos
 def init_app(app):
 # Registrar 'close_db' para que se ejecute al final del contexto de la aplicación
--- entornoTPbackend/run.py
+++ entornoTPbackend/run.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from app.database import init_app
 from app.views import *
 
 app = Flask(__name__)
 app.route('/', methods=['GET'])(index)
+init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
